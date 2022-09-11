dee03179c13999cb90d7835e2ed79e9e4f02136a
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-09-10 21:27:27 -0600

    added .env .gitignore config

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
+      host: "127.0.0.1",
+      dialect: "mysql",
       dialectOptions: {
         decimalNumbers: true,
       },
